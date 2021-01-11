505717ae5b9867f85af4008895f4af88820a04fc
zmej-gorunuch <z.g-web@ukr.net>
2021-01-11 09:04:11 +0200

    update links

--- app/js/main.js
+++ app/js/main.js
@@ -158,3 +158,18 @@ function opentab1(tabName) {
   document.getElementById(tabName).style.display = "block";  
 }
 
+// scroll 
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}

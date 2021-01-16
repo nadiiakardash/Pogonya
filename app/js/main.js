843e6f192422040a2b2dd4e0ba058ca30b5fe879
zmej-gorunuch <z.g-web@ukr.net>
2021-01-16 16:38:56 +0200

    updates

--- app/js/main.js
+++ app/js/main.js
@@ -13,7 +13,6 @@ $(window).scroll(function() {
 });
 
 AOS.init();
-
 // You can also pass an optional settings object
 // below listed default settings
 AOS.init({
@@ -26,7 +25,6 @@ AOS.init({
   disableMutationObserver: false, // disables automatic mutations' detections (advanced)
   debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
   throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-  
 
   // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
